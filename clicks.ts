39ee3a17bf81f1e8a47c32f53aefedf44600d0bc
Pear-News <pearnews.forqc.1c@gmail.com>
2025-02-23 23:06:39 +0800

    Update clicks.ts

--- clicks.ts
+++ clicks.ts
@@ -55,7 +55,7 @@ function buttonHandler(i: number) {
     } else {
         abPressed = false
     }
-    serial.write(`abPressed: ${abPressed}`)
+    serial.writeLine(`abPressed: ${abPressed}`)
 
     i--;  // Adjust to 0-based AorB and array index.
 
@@ -70,7 +70,7 @@ function buttonHandler(i: number) {
             if ((lastClickEnd[i] > 0) && (currentTime - lastClickEnd[i] < doubleClickTime)) {
                 lastClickEnd[i] = 0
                 if (abPressed) { i = AorB.AB }
-                serial.write(`Double Click: ${i}`)
+                serial.writeLine(`Double Click: ${i}`)
                 doActions(i, DOUBLECLICK)
             } else {
                 if(inLongClick[i]) {
@@ -92,7 +92,7 @@ loops.everyInterval(singleClickCheckTime, function() {
         if ((lastClickEnd[i] > 0) && (currentTime - lastClickEnd[i] > doubleClickTime)) {
             lastClickEnd[i] = 0
             if (abPressed) { i = AorB.AB }
-            serial.write(`Single Click: ${i}`)
+            serial.writeLine(`Single Click: ${i}`)
             doActions(i, SINGLECLICK)
         }
         
@@ -103,7 +103,7 @@ loops.everyInterval(singleClickCheckTime, function() {
             inLongClick[i] = true
             lastPressedStart[i] = currentTime
             if (abPressed) { i = AorB.AB }
-            serial.write(`Long Click: ${i}`)
+            serial.writeLine(`Long Click: ${i}`)
             doActions(i, LONGCLICK)
         }
     }
@@ -128,7 +128,7 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB,
 export function onButtonSingleClicked(button: AorB, body: Action) {
     let buttonHandlers = actions.get(button)
     buttonHandlers.set(SINGLECLICK, body)
-    serial.write(`Single Click, ${body}`)
+    serial.writeLine(`Single Click, ${body}`)
 }
 
 //% blockId=onButtonDoubleClicked block="on button |%NAME double clicked "
@@ -136,7 +136,7 @@ export function onButtonSingleClicked(button: AorB, body: Action) {
 export function onButtonDoubleClicked(button: AorB, body: Action) {
     let buttonHandlers = actions.get(button)
     buttonHandlers.set(DOUBLECLICK, body)
-    serial.write(`Double Click, ${body}`)
+    serial.writeLine(`Double Click, ${body}`)
 }
 
 //% blockId=onButtonHeld block="on button |%NAME held"
@@ -144,7 +144,7 @@ export function onButtonDoubleClicked(button: AorB, body: Action) {
 export function onButtonHeld(button: AorB, body: Action) {
     let buttonHandlers = actions.get(button)
     buttonHandlers.set(LONGCLICK, body)
-    serial.write(`Long Click, ${body}`)
+    serial.writeLine(`Long Click, ${body}`)
 }
 
 
